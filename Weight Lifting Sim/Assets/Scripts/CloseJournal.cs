94f8d1a18d85257fccd4cdb52120653fcacd2b5a
weijiej2964 <73482897+weijiej2964@users.noreply.github.com>
2023-01-04 19:20:37 -0500

    Set-Basic-Design

--- Weight Lifting Sim/Assets/Scripts/CloseJournal.cs
+++ Weight Lifting Sim/Assets/Scripts/CloseJournal.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class CloseJournal : MonoBehaviour
 {
     public GameObject Journal;
+    public GameObject CloseButton;
+    public GameObject OpenButton;
     public void ButtonClicked()
     {
         Journal.SetActive(false);
+        CloseButton.SetActive(false);
+        OpenButton.SetActive(true);
     }
 }
--- Weight Lifting Sim/Assets/Scripts/OpenJournal.cs
+++ Weight Lifting Sim/Assets/Scripts/OpenJournal.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class OpenJournal : MonoBehaviour
 {
     public GameObject Journal;
+    public GameObject CloseButton;
+    public GameObject OpenObject;
     public void ButtonClicked()
     {
         Journal.SetActive(true);
+        CloseButton.SetActive(true);
+        OpenObject.SetActive(false);
     }
 }
