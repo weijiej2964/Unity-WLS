ce472280d2b833c03b42f18f4f27d59e5214d098
weijiej2964 <73482897+weijiej2964@users.noreply.github.com>
2023-01-09 15:02:19 -0500

    Open/Exit-Journal-Buttons

--- Weight Lifting Sim/Assets/Scripts/CloseJournal.cs
+++ Weight Lifting Sim/Assets/Scripts/CloseJournal.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CloseJournal : MonoBehaviour
 {
     public GameObject Journal;
     public GameObject CloseButton;
     public GameObject OpenButton;
+
+    [SerializeField] private Image Background;
+    [SerializeField] private Color BackgroundColor;
+
+    private void Start()
+    {
+        BackgroundColor.a = 1;
+    }
     public void ButtonClicked()
     {
         Journal.SetActive(false);
         CloseButton.SetActive(false);
         OpenButton.SetActive(true);
+        Background.color = BackgroundColor;
     }
 }
--- Weight Lifting Sim/Assets/Scripts/OpenJournal.cs
+++ Weight Lifting Sim/Assets/Scripts/OpenJournal.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenJournal : MonoBehaviour
 {
     public GameObject Journal;
     public GameObject CloseButton;
     public GameObject OpenObject;
+
+    [SerializeField] private Image Background;
+    [SerializeField] private Color BackgroundColor;
+
+    private void Start()
+    {
+        BackgroundColor.a = 1; 
+    }
+  
+
+   
     public void ButtonClicked()
     {
         Journal.SetActive(true);
         CloseButton.SetActive(true);
         OpenObject.SetActive(false);
+        Background.color = BackgroundColor; 
     }
+   
 }
